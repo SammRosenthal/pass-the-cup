6c5e25cd5e17077aa03dd95d4a56e465d055276a
sammrosenthal <samrrosenthal@gmail.com>
2022-08-23 20:11:28 -0400

    init logic for baseball game

--- src/server/router/mlbRouter.ts
+++ src/server/router/mlbRouter.ts
@@ -3,6 +3,7 @@ import { createRouter } from "./context";
 import { getAllGames } from "../../api/mlb/getAllGames";
 import { getBoxScore } from "../../api/mlb/getBoxScore";
 import { getPlayByPlay } from "../../api/mlb/getPlayByPlay";
+import { BoxScore } from "../../utils/mlb/shapes";
 
 export const mlbRouter = createRouter()
   .query("allGames", {
@@ -30,6 +31,36 @@ export const mlbRouter = createRouter()
 //   },
 // });
 
+/** Planning stuff to make the game */
+  const currData: BoxScore = {} as BoxScore;
+  const prevData: BoxScore = {} as BoxScore;
+
+  let currentTeamAtBat = currData.TeamGames.find(({ TeamID }) => TeamID === currData.Game.CurrentHittingTeamID); // currData.Game.CurrentHittingTeamID;
+
+  if (currData.Game.CurrentHitterID !== prevData.Game.CurrentHitterID) {
+    if (currData.Game.Outs !== prevData.Game.Outs) {
+      if (currentTeamAtBat?.Strikeouts) {
+        // this should be the minus -2
+        // for a strike out
+        // return????
+      }
+      // I think this can be the minus -1 for another out while in play
+      // return???????
+    }
+
+    // handle hits
+    if (currData.Game.RunnerOnFirst) {
+      // single was hit +1
+    } else if (currData.Game.RunnerOnSecond) {
+      // double was hit +2
+    } else if (currData.Game.RunnerOnThird) {
+      // triple was hit +3
+    } else {
+      // home run +the value of cup
+    }
+  }
+/** Planning stuff to make the game */
+
 //  RULES FOR BASEBALL POINT TRACKING
 //   ------------------------------
 //   | (-) means put money in cup |
@@ -37,7 +68,7 @@ export const mlbRouter = createRouter()
 //   ------------------------------
 //  -- Strikeout: -2
 //  -- Fly/ground out: -1
-//  -- Ejection: -20
+//  -- Ejection: -20 -- Don't think we will have access to this data
 //  -- Single: +1
 //  -- Double: +2
 //  -- Triple: +3
