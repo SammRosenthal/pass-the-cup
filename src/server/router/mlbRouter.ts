5ccaf84ea44fd009e56fb2ccf1ec6894c852a87b
sammrosenthal <samrrosenthal@gmail.com>
2022-08-24 19:00:37 -0400

    fix

--- src/server/router/mlbRouter.ts
+++ src/server/router/mlbRouter.ts
@@ -32,48 +32,48 @@ export const mlbRouter = createRouter()
 //   },
 // });
 
-/** Planning stuff to make the game */
-  const currData: BoxScore = {} as BoxScore;
-  const prevData: BoxScore = {} as BoxScore;
+// /** Planning stuff to make the game */
+//   const currData: BoxScore = {} as BoxScore;
+//   const prevData: BoxScore = {} as BoxScore;
 
-  let currentTeamAtBat = currData.TeamGames.find(({ TeamID }) => TeamID === currData.Game.CurrentHittingTeamID); // currData.Game.CurrentHittingTeamID;
+//   let currentTeamAtBat = currData.TeamGames.find(({ TeamID }) => TeamID === currData.Game.CurrentHittingTeamID); // currData.Game.CurrentHittingTeamID;
 
-  if (currData.Game.CurrentHitterID !== prevData.Game.CurrentHitterID) {
-    if (currData.Game.Outs !== prevData.Game.Outs) {
-      if (currentTeamAtBat?.Strikeouts) {
-        // strikeout
-        // return -2;
-      }
-      // ground/pop out
-      // return -1;
-    }
+//   if (currData.Game.CurrentHitterID !== prevData.Game.CurrentHitterID) {
+//     if (currData.Game.Outs !== prevData.Game.Outs) {
+//       if (currentTeamAtBat?.Strikeouts) {
+//         // strikeout
+//         // return -2;
+//       }
+//       // ground/pop out
+//       // return -1;
+//     }
 
-    // handle hits
-    if (currData.Game.RunnerOnFirst) {
-      // single
-      // return 1;
-    } else if (currData.Game.RunnerOnSecond) {
-      // double
-      // return 2;
-    } else if (currData.Game.RunnerOnThird) {
-      // triple
-      // return 3;
-    } else {
-      // home run
-      // return cupValue;
-    }
-  }
-/** Planning stuff to make the game */
+//     // handle hits
+//     if (currData.Game.RunnerOnFirst) {
+//       // single
+//       // return 1;
+//     } else if (currData.Game.RunnerOnSecond) {
+//       // double
+//       // return 2;
+//     } else if (currData.Game.RunnerOnThird) {
+//       // triple
+//       // return 3;
+//     } else {
+//       // home run
+//       // return cupValue;
+//     }
+//   }
+// /** Planning stuff to make the game */
 
-//  RULES FOR BASEBALL POINT TRACKING
-//   ------------------------------
-//   | (-) means put money in cup |
-//   | (+) means take money out   |
-//   ------------------------------
-//  -- Strikeout: -2
-//  -- Fly/ground out: -1
-//  -- Ejection: -20 -- Don't think we will have access to this data
-//  -- Single: +1
-//  -- Double: +2
-//  -- Triple: +3
-//  -- Home run: take the cup
+// //  RULES FOR BASEBALL POINT TRACKING
+// //   ------------------------------
+// //   | (-) means put money in cup |
+// //   | (+) means take money out   |
+// //   ------------------------------
+// //  -- Strikeout: -2
+// //  -- Fly/ground out: -1
+// //  -- Ejection: -20 -- Don't think we will have access to this data
+// //  -- Single: +1
+// //  -- Double: +2
+// //  -- Triple: +3
+// //  -- Home run: take the cup
